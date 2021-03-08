5315a256e509b493a56df5cc5fbb8c779a83c596
kalifun <kalifun@163.com>
2021-03-08 19:47:15 +0800

    :sparkles: init

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/kalifun/common"
+	"github.com/kalifun/merry/common"
 )
 
 func main()  {
-	fmt.Sprintf(common.Logo, "\x1b[35m", "\x1b[1m",
-		"\x1b[0m", "\x1b[32m", "\x1b[1m", "\x1b[0m")
+	fmt.Println(common.Logo)
 }
